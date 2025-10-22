2e725e5e752e415c07b8ac98ff8c5889f97c0007
DAVIKAP <davikaptzki@yahoo.com.br>
2025-10-22 18:45:38 -0300

    Desafio Batalha Naval Aventureiro Completo
    Implement positioning of ships on a 10x10 board, including horizontal, vertical, and diagonal placements.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+    // Criação do Tabuleiro 10x10
 
     char colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
 
@@ -31,27 +25,45 @@ int main() {
         {0,0,0,0,0,0,0,0,0,0}
         };
 
-        int navioHor[] = {0,0,3,3,3,0,0,0,0,0};
-        int navioVert[] = {0,0,0,0,3,3,3,0,0,0};
-
+        // Posicionamento dos navios no tabuleiro 
+        
         int i;
         int j;
         
+        // Navio horizontal
+
         for (j = 2; j < 5; j++)
         {
-        tabuleiro[1][j] = navioHor[j];
-        }
-        
-        for (i = 4; i < 8; i++)
-        {
-        tabuleiro[i][8] = navioVert[i];
+        tabuleiro[1][j] = 3;
         }
 
+        // Navio vertical
+        
+        for (i = 5; i < 8; i++)
+        {
+        tabuleiro[i][8] = 3;
+        }
+
+        // Navios diagonais
+
+        for (i = 3, j = 3; i < 6, j < 6; i++, j++)
+        {
+        tabuleiro[i][j] = 3;
+        }
+
+        for (i = 3, j = 3; i < 6, j < 6; i++, j++)
+        {
+        tabuleiro[i][j] = 3;
+        }
+
+        for (i = 7, j = 2; i < 10, j >= 0; i++, j--)
+        {
+        tabuleiro[i][j] = 3;
+        }
+        
+       // Impressão do tabuleiro com navios posicionados
+
         for(i = 0; i < 10; i++) {
-            if (i == 0) 
-            {
-                continue;
-            }
             printf("%d", i);
             for(j = 0; j < 10; j++){
                 printf("%d", tabuleiro[i][j]);
@@ -64,6 +76,9 @@ int main() {
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+
+
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
