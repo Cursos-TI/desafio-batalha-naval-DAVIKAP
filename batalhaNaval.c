7e0dc5faae82993ce9a3bed654713be0b50c7274
DAVIKAP <davikaptzki@yahoo.com.br>
2025-10-24 21:18:29 -0300

    Desafio Batalha Naval Nível Mestre Finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -8,7 +8,7 @@ int main() {
 
         printf(" ");
         for(int i = 0; i < 10; i++) {
-            printf("%c", colunas[i]);
+            printf(" %c", colunas[i]);
         };   
         printf("\n");       
  
@@ -31,15 +31,15 @@ int main() {
         int j;
         
         // Navio horizontal
-
-        for (j = 2; j < 5; j++)
+        
+        for (j = 0; j < 3; j++)
         {
-        tabuleiro[1][j] = 3;
+        tabuleiro[5][j] = 3;
         }
 
         // Navio vertical
         
-        for (i = 5; i < 8; i++)
+        for (i = 4; i < 7; i++)
         {
         tabuleiro[i][8] = 3;
         }
@@ -61,45 +61,43 @@ int main() {
         tabuleiro[i][j] = 3;
         }
         
+        for(i = 0; i < 3; i++) {
+           for(j = 0; j < 5; j++){
+              if (j >= 2 - i && j <= 2 + i){
+                tabuleiro[i][j] = 5;
+                }
+           }
+        }
+                
+        for(i = 7; i < 10; i++) {
+           for(j = 5; j < 10; j++){ 
+              if ((i == 7 || i == 9) && (j < 7 || j > 7)){
+                tabuleiro[i][j] = 0;
+                } else {
+                tabuleiro[i][j] = 5;    
+                }
+           }
+        }
+
+        for(i = 0; i < 3; i++) {
+           for(j = 7; j < 10; j++){ 
+              if ((i == 0 || i == 2) && (j < 8 || j > 8)){
+                tabuleiro[i][j] = 0;
+                } else {
+                tabuleiro[i][j] = 5;    
+                }
+           }
+        }
+
        // Impressão do tabuleiro com navios posicionados
 
         for(i = 0; i < 10; i++) {
             printf("%d", i);
             for(j = 0; j < 10; j++){
-                printf("%d", tabuleiro[i][j]);
+                printf(" %d", tabuleiro[i][j]);
             }
             printf("\n");       
         }
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-
-
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
